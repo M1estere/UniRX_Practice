c23fc9df815a8a29ba308e7661c021dea7af0d5a
Ilya_Solov <7156643@gmail.com>
2024-04-15 17:32:09 +0300

    Implemented sfx and music controls

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,14 +1,43 @@
+using UniRx;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject _soundCross;
+    [SerializeField] private GameObject _musicCross;
+        
+    private ReactiveProperty<bool> _soundOn = new (true);
+    private ReactiveProperty<bool> _musicOn = new (true);
+    
     private void Awake()
     {
         Application.targetFrameRate = -1;
         Time.timeScale = 1;
+
+        _soundOn.Subscribe(delegate(bool b)
+        {
+            _soundCross.SetActive(!b);
+        });
+
+        _musicOn.Subscribe(delegate(bool b)
+        {
+            _musicCross.SetActive(!b);
+        });
     }
 
+    public void ToggleValue(int type)
+    {
+        if (type == 0)
+        {
+            _soundOn.Value = !_soundOn.Value;
+        }
+        else
+        {
+            _musicOn.Value = !_musicOn.Value;
+        }
+    }
+    
     public void OpenLevel() => SceneManager.LoadScene(1);
     
     public void QuitGame() => Application.Quit();
